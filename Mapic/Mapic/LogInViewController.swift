4aad901ef6b5cb149c45a9748ffa9d7bc9636117
Suyeon Kang <skang23@wustl.edu>
2016-03-01 18:15:15 -0600

    update

---
+++ Mapic/Mapic/LogInViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LogInViewController.swift
+//  Mapic
+//
+//  Created by Suyeon Kang on 3/1/16.
+//  Copyright © 2016 Suyeon Kang. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func signInClicked(sender: AnyObject)
+    {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: NSError?) -> Void in
+            if let error = error {
+                print("User login failed.")
+                print(error.localizedDescription)
+            } else {
+                print("User logged in successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+
+            }
+         
+            
+        }
+    }
+    @IBAction func signUpClicked(sender: AnyObject) {
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.signUpInBackgroundWithBlock { (success:Bool, error:NSError?) -> Void in
+            if success {
+              print("Yay, created a user!")
+            } else {
+                print(error?.localizedDescription)
+                if error?.code == 202 {
+                    print("Username is taken")
+                }
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ Mapic/Mapic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mapic
+//
+//  Created by Suyeon Kang on 3/1/16.
+//  Copyright © 2016 Suyeon Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
